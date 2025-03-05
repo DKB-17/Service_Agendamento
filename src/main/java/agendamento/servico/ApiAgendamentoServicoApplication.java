5ca704494040f095f5bb5b1c0fd680a8d702b8f0
diego <136401880+DKB-17@users.noreply.github.com>
2025-03-05 15:36:57 -0300

    feat: Initial Commit
    Comit inicial do projeto

---
+++ src/main/java/agendamento/servico/ApiAgendamentoServicoApplication.java
@@ -0,0 +1,13 @@
+package agendamento.servico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAgendamentoServicoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiAgendamentoServicoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/agendamento/servico/ApiAgendamentoServicoApplicationTests.java
@@ -0,0 +1,13 @@
+package agendamento.servico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAgendamentoServicoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
