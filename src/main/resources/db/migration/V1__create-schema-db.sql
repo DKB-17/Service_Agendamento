f61396ada6bccd9082f1ad7e8be9fc073c7b86db
diego <136401880+DKB-17@users.noreply.github.com>
2025-03-05 15:40:36 -0300

    feat: Criar schema do banco de dados
    criacao do schema do banco de dados com as entidade, barbeiro, horario, servico, caixa, agenda e cliente

---
+++ src/main/resources/db/migration/V1__create-schema-db.sql
@@ -0,0 +1,86 @@
+CREATE TABLE "horarios" (
+                            "id" SERIAL PRIMARY KEY,
+                            "inicio" time,
+                            "fim" time,
+                            "delete_at" timestamp,
+                            "update_at" timestamp,
+                            "created_at" timestamp
+);
+
+CREATE TABLE "barbeiros" (
+                             "id" SERIAL PRIMARY KEY,
+                             "nome" varchar(100),
+                             "link_imagem" varchar(255),
+                             "delete_at" timestamp,
+                             "update_at" timestamp,
+                             "created_at" timestamp
+);
+
+CREATE TABLE "horario_barbeiro" (
+                                    "horario_id" integer,
+                                    FOREIGN KEY (horario_id) REFERENCES horarios(id),
+                                    "barbeiro_id" integer,
+                                    FOREIGN KEY (barbeiro_id) REFERENCES barbeiros(id)
+);
+
+CREATE TABLE "servicos" (
+                            "id" SERIAL PRIMARY KEY,
+                            "descricao" varchar(50),
+                            "valor" numeric(7,2),
+                            "duracao" time,
+                            "delete_at" timestamp,
+                            "update_at" timestamp,
+                            "created_at" timestamp
+);
+
+CREATE TABLE "servico_barbeiro" (
+                                    "barbeiro_id" integer,
+                                    FOREIGN KEY (barbeiro_id) REFERENCES barbeiros(id),
+                                    "servico_id" integer,
+                                    FOREIGN KEY (servico_id) REFERENCES servicos(id)
+);
+
+CREATE TABLE "clientes" (
+                            "id" SERIAL PRIMARY KEY,
+                            "nome" varchar(100),
+                            "cpf" varchar(14),
+                            "contato" varchar(14),
+                            "delete_at" timestamp,
+                            "update_at" timestamp,
+                            "created_at" timestamp
+);
+
+CREATE TABLE "caixas" (
+                          "id" SERIAL PRIMARY KEY,
+                          "lucro" numeric(10,2),
+                          "dia" date,
+                          "delete_at" timestamp,
+                          "update_at" timestamp,
+                          "created_at" timestamp
+);
+
+CREATE TYPE etapa AS ENUM ('pendente', 'concluido', 'cancelado', 'remarcado');
+
+CREATE TABLE "agendas" (
+                           "id" SERIAL PRIMARY KEY,
+                           "cliente_id" integer,
+                            FOREIGN KEY (cliente_id) REFERENCES clientes(id),
+                           "horario_id" integer,
+                            FOREIGN KEY (horario_id) REFERENCES horarios(id),
+                           "barbeiro_id" integer,
+                            FOREIGN KEY (barbeiro_id) REFERENCES barbeiros(id),
+                           "servico_id" integer,
+                            FOREIGN KEY (servico_id) REFERENCES servicos(id),
+                           "caixa_id" integer,
+                            FOREIGN KEY (caixa_id) REFERENCES "caixas"(id),
+                           "dia" date,
+                           "valor" numeric(7,2),
+                           "etapa" etapa,
+                           "delete_at" timestamp,
+                           "update_at" timestamp,
+                           "created_at" timestamp
+);
+
+
+
+
