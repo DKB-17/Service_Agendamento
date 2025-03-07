d6e89629519a1eb25b3d46291040c65726dfbd03
DKB-17 <diegobritosilva17@gmail.com>
2025-03-07 16:00:52 -0300

    feat: Criar o DTO de Cadastro de informacoes de Barbeiro
    Criacao do record para validacao dos dados do body da request com as informacoes para cadastras.

---
+++ src/main/java/agendamento/servico/dto/CadastroBarbeiro.java
@@ -0,0 +1,11 @@
+package agendamento.servico.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record CadastroBarbeiro(
+        @NotBlank
+        String nome,
+        @NotBlank
+        String caminhoImagem
+) {
+}
