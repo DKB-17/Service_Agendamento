c0d546e6d8d470ce80b5f9d7735d2bf9c6427084
DKB-17 <diegobritosilva17@gmail.com>
2025-03-07 15:58:22 -0300

    feat: Criar herança de BarbeiroRepository com JPA
    Vinculo de heranca de BarbeiroRepository com pacote JPA e criacao do metodo especifico de FindByDeleteAtIsNull, onde extrai uma lista de Entidades do banco onde nao possua informacao na coluna delete_at.

---
+++ src/main/java/agendamento/servico/repository/BarbeiroRepository.java
@@ -0,0 +1,11 @@
+package agendamento.servico.repository;
+
+import agendamento.servico.dto.RegistroBarbeiro;
+import agendamento.servico.entity.Barbeiro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BarbeiroRepository extends JpaRepository<Barbeiro, Long> {
+        List<Barbeiro> findByDeleteAtIsNull();
+}
