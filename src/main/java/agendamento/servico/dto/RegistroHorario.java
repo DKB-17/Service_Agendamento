1a1e284cc94a4c0216e510ad9e1a737d5505ec8a
DKB-17 <diegobritosilva17@gmail.com>
2025-03-10 16:22:45 -0300

    feat: Criar o DTO de Registro de informacoes de Horario
    Criacao do record para transferencia das informacoes da entidade Horario para a disponibilidade externa da aplicação.

---
+++ src/main/java/agendamento/servico/dto/RegistroHorario.java
@@ -0,0 +1,12 @@
+package agendamento.servico.dto;
+
+import java.time.Instant;
+import java.time.LocalTime;
+
+public record RegistroHorario(
+        Long id,
+        LocalTime horarioInicio,
+        LocalTime horarioFim,
+        Instant createAt
+) {
+}
