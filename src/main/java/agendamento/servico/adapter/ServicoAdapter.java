7e7bb1e0c6c611478aa1449e881d14c1c5283234
diego <136401880+DKB-17@users.noreply.github.com>
2025-04-29 16:52:31 -0300

    feat: Criar o Adapter de Servico Criacao da classe ServicoAdapter e implementando a conversao de tipos DTO para entity e entity para DTO.

---
+++ src/main/java/agendamento/servico/adapter/ServicoAdapter.java
@@ -0,0 +1,26 @@
+package agendamento.servico.adapter;
+
+import agendamento.servico.dto.CadastroServico;
+import agendamento.servico.dto.RegistroServico;
+import agendamento.servico.entity.Servico;
+
+import java.time.Instant;
+
+public class ServicoAdapter {
+    public static Servico fromCadastroServicoToEntity(CadastroServico cadastroServico) {
+        return new Servico(
+            null,
+            cadastroServico.descricao(),
+            null,
+            Instant.now(),
+            Instant.now()
+        );
+    }
+    public static RegistroServico fromEntityToRegistroServico(Servico servico) {
+        return new RegistroServico(
+            servico.getId(),
+            servico.getDescricao(),
+            servico.getCreateAt()
+        );
+    }
+}
