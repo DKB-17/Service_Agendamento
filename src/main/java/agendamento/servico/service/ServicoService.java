e9834e5e951e203c14147ffe7f51c1351ee6376f
diego <diegobritosilva17@gmail.com>
2025-04-29 12:15:21 -0300

    Add ServicoService interface for service management
    Introduce the ServicoService interface to define core service operations, including CRUD functionalities and service activation/deactivation. This establishes a contract for service handling in the system.

---
+++ src/main/java/agendamento/servico/service/ServicoService.java
@@ -0,0 +1,17 @@
+package agendamento.servico.service;
+
+import agendamento.servico.dto.CadastroServico;
+import agendamento.servico.dto.RegistroServico;
+
+import java.util.List;
+
+public interface ServicoService {
+
+    public RegistroServico cadastrarServico(CadastroServico dados);
+    public RegistroServico buscarServico(Long id);
+    public void desativarServico(Long id);
+    public RegistroServico ativarServico(Long id);
+    public RegistroServico atualizarServico(Long id, CadastroServico dados);
+    public List<RegistroServico> listarServicos();
+
+}
