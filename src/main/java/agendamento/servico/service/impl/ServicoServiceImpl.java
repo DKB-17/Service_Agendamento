e84066110ff68660b5f2cf470a3a37039bc91222
diego <136401880+DKB-17@users.noreply.github.com>
2025-04-29 16:47:23 -0300

    feat: Implementar os contatos de ServicoService
    Implementacao dos metodos de contrato da interface de contrato ServicoService, com os metodos de CRUD

---
+++ src/main/java/agendamento/servico/service/impl/ServicoServiceImpl.java
@@ -0,0 +1,68 @@
+package agendamento.servico.service.impl;
+
+import agendamento.servico.adapter.ServicoAdapter;
+import agendamento.servico.dto.CadastroServico;
+import agendamento.servico.dto.RegistroServico;
+import agendamento.servico.entity.Servico;
+import agendamento.servico.repository.ServicoRepository;
+import agendamento.servico.service.ServicoService;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+
+public class ServicoServiceImpl implements ServicoService {
+
+    private ServicoRepository servicoRepository;
+
+    @Override
+    public RegistroServico cadastrarServico(CadastroServico dados) {
+        Servico servico = ServicoAdapter.fromCadastroServicoToEntity(dados);
+        servicoRepository.save(servico);
+        return ServicoAdapter.fromEntityToRegistroServico(servico);
+    }
+
+    @Override
+    public RegistroServico buscarServico(Long id) {
+        Optional<Servico> servico = servicoRepository.findById(id);
+        if(servico.isEmpty() || servico.get().getDeleteAt() != null) {
+            throw new RuntimeException();
+        }else{
+            return ServicoAdapter.fromEntityToRegistroServico(servico.get());
+        }
+    }
+
+    @Override
+    public void desativarServico(Long id) {
+        Optional<Servico> servico = servicoRepository.findById(id);
+        if(servico.isEmpty() || servico.get().getDeleteAt() != null) {
+            throw new RuntimeException("Registro de servico nao existe");
+        } else {
+            servico.get().setDeleteAt(Instant.now());
+        }
+    }
+
+    @Override
+    public RegistroServico ativarServico(Long id) {
+        Optional<Servico> servico = servicoRepository.findById(id);
+        if(servico.isEmpty()) {
+            throw new RuntimeException("Registro de servico nao existe");
+        } else {
+            servico.get().setDeleteAt(null);
+            return ServicoAdapter.fromEntityToRegistroServico(servico.get());
+        }
+    }
+
+    @Override
+    public RegistroServico atualizarServico(Long id, CadastroServico dados) {
+        Optional<Servico> servico = servicoRepository.findById(id);
+        if(servico.isEmpty() || servico.get().getDeleteAt() != null) {
+            throw new RuntimeException("Registro de servico nao existe");
+        }
+    }
+
+    @Override
+    public List<RegistroServico> listarServicos() {
+        return List.of();
+    }
+}
