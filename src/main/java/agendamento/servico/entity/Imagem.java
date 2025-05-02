a050c018bf1b7b3f6150a30d16e812ae72090de8
diego <diegobritosilva17@gmail.com>
2025-05-02 13:32:11 -0300

    Add Imagem entity to represent image data
    Introduced a new Imagem entity with JPA annotations for database mapping. This includes fields for ID and image path, leveraging Lombok for boilerplate code reduction. The entity is mapped to the "imagens" table.

---
+++ src/main/java/agendamento/servico/entity/Imagem.java
@@ -0,0 +1,20 @@
+package agendamento.servico.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "imagens")
+@Entity(name = "Imagem")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Imagem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "caminho_link")
+    private String caminho;
+
+}
