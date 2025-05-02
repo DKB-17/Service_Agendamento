b3db6c4ae2dd0d6643cb538b43da68c511a1fb0e
diego <diegobritosilva17@gmail.com>
2025-05-02 13:32:20 -0300

    Add valor and duracao fields to Servico entity
    Introduced BigDecimal 'valor' and Duration 'duracao' fields to the Servico entity. These changes enhance the entity to store service pricing and duration details, aligning with business requirements.

--- src/main/java/agendamento/servico/entity/Servico.java
+++ src/main/java/agendamento/servico/entity/Servico.java
@@ -4,6 +4,8 @@ package agendamento.servico.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.math.BigDecimal;
+import java.time.Duration;
 import java.time.Instant;
 
 @Table(name = "servicos")
@@ -19,6 +21,8 @@ public class Servico {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String descricao;
+    private BigDecimal valor;
+    private Duration duracao;
     @Column(name = "deleted_at")
     private Instant deleteAt;
     @Column(name = "updated_at")
