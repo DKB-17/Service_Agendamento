5a6931f3aeb7264ccf628d2b42847f95eed6c64b
diego <136401880+DKB-17@users.noreply.github.com>
2025-05-08 15:13:51 -0300

    fix: Correcao imcompleta do Adapter e Barbeiro, conversao do DTO para Entity

--- src/main/java/agendamento/servico/adapter/BarbeiroAdapter.java
+++ src/main/java/agendamento/servico/adapter/BarbeiroAdapter.java
@@ -3,19 +3,24 @@ package agendamento.servico.adapter;
 import agendamento.servico.dto.CadastroBarbeiro;
 import agendamento.servico.dto.RegistroBarbeiro;
 import agendamento.servico.entity.Barbeiro;
+import agendamento.servico.entity.Imagem;
 
 import java.time.Instant;
 
 public class BarbeiroAdapter {
     public static Barbeiro fromCadastroBarbeiroToEntity(CadastroBarbeiro dados){
-        return new Barbeiro(
-                null,
-                dados.nome(),
-                dados.caminhoImagem(),
-                null,
-                Instant.now(),
-                Instant.now()
-        );
+
+        Imagem imagem = new Imagem();
+
+        Barbeiro barbeiro = new Barbeiro(
+            null,
+            dados.nome(),
+            null,
+            Instant.now(),
+            Instant.now()
+                                        );
+
+        return barbeiro;
     }
 
     public static RegistroBarbeiro fromEntityToRegistroBarbeiro(Barbeiro barbeiro) {
