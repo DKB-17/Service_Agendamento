d7167179d104a564f5eca81bbe6c76ca25693d02
diego <diegobritosilva17@gmail.com>
2025-05-09 08:55:45 -0300

    Add fields for service value and duration to CadastroServico
    This commit introduces `valor` and `duracao` fields to the `CadastroServico` record. These additions enhance the representation of services by including their monetary value and duration, ensuring better data encapsulation.

--- src/main/java/agendamento/servico/dto/CadastroServico.java
+++ src/main/java/agendamento/servico/dto/CadastroServico.java
@@ -2,8 +2,15 @@ package agendamento.servico.dto;
 
 import jakarta.validation.constraints.NotBlank;
 
+import java.math.BigDecimal;
+import java.time.Duration;
+
 public record CadastroServico(
         @NotBlank
-        String descricao
+        String descricao,
+        @NotBlank
+        BigDecimal valor,
+        @NotBlank
+        Duration duracao
 ) {
 }
